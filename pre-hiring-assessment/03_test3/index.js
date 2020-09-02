ed625fef5064a9231e3ba136cf5f1b7ab86f64b0
icy9377 <icy93770@gmail.com>
2020-09-02 10:15:11 +0900

    25333

--- pre-hiring-assessment/03_test3/index.js
+++ pre-hiring-assessment/03_test3/index.js
@@ -1,3 +1,31 @@
 function test3() {
-  // TODO
+  // TODO: 여기에 코드를 작성합니다.
+ let count= [];
+ //리턴 함수 구역
+ return function fibonacci(){
+   let result = 0;
+   if(count.length === 0){
+     count.push(0);
+     return 0
+    }
+   else if(count.length === 1){
+     count.push(1);
+     return 1
+    }
+   else if(count.length === 2){
+     count.push(1);
+     return 1
+
+    }
+   else{
+       let addNum = count[count.length -2] + count[count.length -1];
+       count.push(addNum);
+    }
+    
+   result = count[count.length-1];
+   
+   return result
+ }
+
+//테스트6 끝나는 곳
 }

36b2ae896859bbc5b2533472b3f0ea2c9cc76453
dawa93 <dawa93@naver.com>
2020-09-02 13:15:35 +0900

    completed test7

--- pre-hiring-assessment/07_test7/index.js
+++ pre-hiring-assessment/07_test7/index.js
@@ -1,3 +1,11 @@
 function test7(array) {
   // TODO
+
+  return array.map(function(arr) {
+    let obj = {};
+    arr.reduce(function(x, y) {
+      obj[y[0]] = y[1];
+    }, {});
+    return obj;
+  });
 }

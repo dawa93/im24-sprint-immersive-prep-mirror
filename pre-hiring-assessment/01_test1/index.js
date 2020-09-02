f9199c78ff529f7818ed5e5c277107e6cc56a6ba
dawa93 <dawa93@naver.com>
2020-09-02 14:30:07 +0900

    finished sprint

--- pre-hiring-assessment/01_test1/index.js
+++ pre-hiring-assessment/01_test1/index.js
@@ -1,24 +1,19 @@
 function test1(str) {
   // TODO: 여기에 코드를 작성합니다.
-  let base = str.split(' ');
+  let base = str.split(" ");
   let result = {};
-  if(str.length === 0){
-    return {}
-  }
-  else{
-  for(let i = 0; i < base.length; i++){
-    if(base[i] === ''){
-      base[i] = 0;
+  if (str.length === 0) {
+    return {};
+  } else {
+    for (let i = 0; i < base.length; i++) {
+      if (base[i] === "") {
+        base[i] = 0;
+      } else if (Object.keys(result).indexOf(base[i]) === -1) {
+        result[base[i]] = 1;
+      } else if (result[base[i]] >= 1) {
+        result[base[i]] = result[base[i]] + 1;
+      }
     }
-    else if(Object.keys(result).indexOf(base[i])=== -1){
-      result[base[i]] = 1;
-    }
-    else if(result[base[i]]>= 1){
-      result[base[i]] = result[base[i]] + 1;
-    }
-   }
-   return result
+    return result;
   }
 }
-
-test('ask a bunch try a bunch get a bunch');

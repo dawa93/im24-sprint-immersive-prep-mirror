bb5f42fc2c33f55192eb2f153ce24f1025b32ecb
icy9377 <icy93770@gmail.com>
2020-09-02 09:07:24 +0900

    test1 go

--- pre-hiring-assessment/01_test1/index.js
+++ pre-hiring-assessment/01_test1/index.js
@@ -1,3 +1,22 @@
 function test1(str) {
-  // TODO
+  // TODO: 여기에 코드를 작성합니다.
+  let base = str.toLowerCase().split(' ');
+  let result = {};
+  if(str.length === 0){
+    return {}
+  }
+  else{
+  for(let i = 0; i < base.length; i++){
+    if(base[i] === ''){
+      base[i] = 0;
+    }
+    else if(Object.keys(result).indexOf(base[i])=== -1){
+      result[base[i]] = 1;
+    }
+    else if(result[base[i]]>= 1){
+      result[base[i]] = result[base[i]] + 1;
+    }
+   }
+   return result
+  }
 }

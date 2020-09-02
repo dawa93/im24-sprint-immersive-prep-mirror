d707b92118c4c835029479433916645a72442085
dawa93 <dawa93@naver.com>
2020-09-02 10:22:18 +0900

    completed test4

--- pre-hiring-assessment/04_test4/index.js
+++ pre-hiring-assessment/04_test4/index.js
@@ -1,3 +1,28 @@
+// function test4(num) {
+//   // TODO
+// }
+
 function test4(num) {
-  // TODO
+  // TODO: 여기에 코드를 작성합니다.
+
+  let numToStr = String(num);
+  let arr = numToStr.split("");
+
+  let result = arr.reduce((x, y) => x * y, 1);
+
+  if (result < 10) {
+    return result;
+  } else {
+    return test3(result);
+  }
+
+  // let result = 1;
+  // for(let i =0; i < numToStr.length; i++){
+  //   let arr = numToStr.split('');
+  //   result = result * arr[i];
+
+  //   if(arr.length === 1){
+  //     return arr[0];
+  //   }
+  // }
 }
